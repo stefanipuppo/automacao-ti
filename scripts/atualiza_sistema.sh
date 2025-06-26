c443a5fda73e4c459f3490d5678495e293589f50
Stefani Puppo <stefanipuppo@hotmail.com>
2025-06-26 18:27:05 -0300

    Corrige nome do log e atualiza script de atualização

--- scripts/atualiza_sistema.sh
+++ scripts/atualiza_sistema.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Nome do log
-LOG_FILE="../atualizaco_log.txt"
+LOG_FILE="../atualizacao_log.txt"
 
 # Data e hora
 echo "-------------------" >> "$LOG_FILE"
@@ -19,3 +19,13 @@ sudo apt upgrade -y >> "$LOG_FILE" 2>&1
 #Fim 
 echo " Atualizacao finalizada em: $(date)" >> "LOG_FILE"
 echo "===================================" >> "$LOG_FILE"
+
+
+# Sincroniza a pasta local com o remoto usando rclone
+echo "Iniciando backup com rclone..." >> "$LOG_FILE"
+rclone copy /home/stef/automacao-ti/scripts meudrive:backup --log-file="$LOG_FILE" --log-level INFO
+
+# Fim
+echo "Atualização finalizada em: $(date)" >> "$LOG_FILE"
+echo "===================================" >> "$LOG_FILE"
+
